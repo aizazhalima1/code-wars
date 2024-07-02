633c602462aa623fe06b4b1c179687e89db415b5
aizazhalima1 <halimaaizaz@hotmail.com>
2024-07-02 03:29:19 -0400

    Returns reversed string

---
+++ js/reversedStrings.js
@@ -0,0 +1,12 @@
+//Complete the solution so that it reverses the string passed into it.
+
+
+
+
+
+//Solution
+function solution(str){
+    let word=str.split('').reverse().join('')
+    return word
+      
+      }
