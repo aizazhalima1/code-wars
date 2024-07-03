d83ca7bdcd851e1fc71a1e3260b5a8e18fb93499
aizazhalima1 <halimaaizaz@hotmail.com>
2024-07-03 04:04:12 -0400

    return smallest integer from an array

---
+++ js/smallestInteger.js
@@ -0,0 +1,12 @@
+//Given an array of integers your solution should find the smallest integer.
+
+
+
+
+
+//Solution
+function findSmallestInt(arr) {
+    //your code here
+   return Math.min(...arr)
+   
+  }
