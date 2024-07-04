7790e8be15bcff1b605b81e78bb9b3c397778263
aizazhalima1 <halimaaizaz@hotmail.com>
2024-07-04 03:51:12 -0400

    Remove first and last characters of a string

---
+++ js/removeFirstAndLast.js
@@ -0,0 +1,14 @@
+//It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, 
+//the original string. You don't have to worry about strings with less than two characters.
+
+
+
+
+
+//Solution
+function removeChar(str){
+    //You got this!
+     let newStr=str.substring(1,str.length-1)
+     return newStr
+   
+   };
