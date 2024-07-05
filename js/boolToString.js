9e92ec58106e236c6ed0a78bf4ce5ee58f842975
aizazhalima1 <halimaaizaz@hotmail.com>
2024-07-05 05:04:01 -0400

    Convert boolean value to string

---
+++ js/boolToString.js
@@ -0,0 +1,11 @@
+//Implement a function which convert the given boolean value into its string representation.
+
+
+
+
+
+//Solution
+function booleanToString(b){
+    //your code here
+    return b.toString()
+  }
