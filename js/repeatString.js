fbdcf060b861e38e7e2738c3d9e251b2e54a4035
aizazhalima1 <halimaaizaz@hotmail.com>
2024-07-24 01:45:10 -0400

    Repeat a string n times

---
+++ js/repeatString.js
@@ -0,0 +1,10 @@
+//Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+
+
+
+
+//Solution
+function repeatStr (n, s) {
+    return s.repeat(n);
+  }
