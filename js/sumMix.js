c1491fa315f1a9426222cb3fde08179c96482ba7
aizazhalima1 <halimaaizaz@hotmail.com>
2024-08-05 01:03:20 -0400

    Return sum of array values as if they were all numbers

---
+++ js/sumMix.js
@@ -0,0 +1,12 @@
+//Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+//Return your answer as a number.
+
+
+
+
+
+//Solution
+function sumMix(x){
+    return x.reduce((acc,c)=> acc + Number(c), 0,)
+    }
