2a00239bdfc44550188f7f1cd6c9199577b0da05
aizazhalima1 <halimaaizaz@hotmail.com>
2024-08-07 03:00:06 -0400

    Return string with spaces removed

---
+++ js/noSpace.js
@@ -0,0 +1,12 @@
+//Write a function that removes the spaces from the string, then return the resultant string.
+
+
+
+
+
+//Solution
+function noSpace(x){
+    x=x.replaceAll(' ','')
+    return x
+  
+  }
