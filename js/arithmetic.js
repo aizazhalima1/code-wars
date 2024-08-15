f36e07f7106fff4a9dbfd514cc32bc0d7f5646b5
aizazhalima1 <halimaaizaz@hotmail.com>
2024-08-15 02:07:38 -0400

    Return result of two numbers having specific arithmetic operation on them

---
+++ js/arithmetic.js
@@ -0,0 +1,20 @@
+//Given two numbers and an arithmetic operator (the name of it, as a string), return the result of the two numbers having that operator used on them.
+
+
+
+
+
+//Solution
+function arithmetic(a, b, operator){
+    switch (operator){
+    case 'add':
+      return a+b
+    case 'subtract':
+      return a-b
+    case 'multiply':
+     return a*b
+     case 'divide':
+     return a/b
+    default:
+  }
+  }
