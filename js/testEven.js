7d4310ad39fcedd7b0543adad53c639ea17ce19b
aizazhalima1 <halimaaizaz@hotmail.com>
2024-08-16 00:17:18 -0400

    Return if number is even or odd

---
+++ js/testEven.js
@@ -0,0 +1,22 @@
+//n this Kata we are passing a number (n) into a function.
+
+//Your code will determine if the number passed is even (or not).
+
+//The function needs to return either a true or false.
+
+//Numbers may be positive or negative, integers or floats.
+
+
+
+
+
+//Solution
+function testEven(n) {
+    //Your awesome code here!
+  if(n%1!=0 || n%2!=0){
+    return false
+  }
+  else{
+    return true
+  }
+}
