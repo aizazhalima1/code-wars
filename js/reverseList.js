b94d1a47f55b3bab058831b4e0d481a3da060dab
aizazhalima1 <halimaaizaz@hotmail.com>
2024-08-20 04:58:27 -0400

    Return list in reversed order

---
+++ js/reverseList.js
@@ -0,0 +1,10 @@
+//In this kata you will create a function that takes in a list and returns a list with the reverse order.
+
+
+
+
+
+//Solution
+function reverseList(list) {
+    return list.reverse()
+  }
