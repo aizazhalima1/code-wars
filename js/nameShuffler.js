678f0404a8d559cb9ab2efbc73892ed7df66d943
aizazhalima1 <halimaaizaz@hotmail.com>
2024-08-22 03:36:01 -0400

    Return string with first name swapped with last name

---
+++ js/nameShuffler.js
@@ -0,0 +1,10 @@
+//Write a function that returns a string in which firstname is swapped with last name.
+
+
+
+
+
+//Solution
+function nameShuffler(str){
+    return str.split(' ').reverse().join(' ')
+  }
