ff0c33939093e9e1fa3449eba3203f5d7be97726
aizazhalima1 <halimaaizaz@hotmail.com>
2024-08-24 01:11:20 -0400

    Return additive inverse of each element

---
+++ js/invert.js
@@ -0,0 +1,11 @@
+//Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+
+
+
+
+//Solution
+function invert(array) {
+    array=array.map((x)=> -1*x)
+   return array
+ }
