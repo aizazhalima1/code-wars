15820accea2c70028625c0c829fc7b3fa8390629
aizazhalima1 <halimaaizaz@hotmail.com>
2024-08-31 00:21:28 -0400

    Return sum of goals

---
+++ js/grassHop.js
@@ -0,0 +1,12 @@
+//Use variables to find the sum of the goals Messi scored in 3 competitions
+
+
+
+
+
+//Solution
+var laLigaGoals = 43
+var championsLeagueGoals = 10
+var copaDelReyGoals = 5
+
+var totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals
