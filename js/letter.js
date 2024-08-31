be72268c515719bbac4cc3d9ce27c144280e8c9d
aizazhalima1 <halimaaizaz@hotmail.com>
2024-08-31 00:19:38 -0400

    Return position of letter in alphabet

---
+++ js/letter.js
@@ -0,0 +1,12 @@
+//When provided with a letter, return its position in the alphabet.
+
+
+
+
+
+//Solution
+function position(letter){
+
+      let alp='abcdefghijklmnopqrstuvwxyz'
+      return `Position of alphabet: ${(alp.indexOf(letter)) + 1}`
+    }
