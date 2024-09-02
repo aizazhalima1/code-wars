9b6749c9fce33f3fcd00d46aa45caec1c47178c2
aizazhalima1 <halimaaizaz@hotmail.com>
2024-09-02 02:19:18 -0400

    Return true if plural should be used with input number

---
+++ js/plural.js
@@ -0,0 +1,11 @@
+//We need a simple function that determines if a plural is needed or not. It should take a number, and return true if a plural should be used with that number or false if not.
+
+
+
+
+
+//Solution
+function plural(n) {
+    return n==1? false : true
+    // ...
+  }
