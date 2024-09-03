12fdca4959d3bd346c4dffeb8840a44bd8d0631e
aizazhalima1 <halimaaizaz@hotmail.com>
2024-09-03 00:20:43 -0400

    Return true if exactly one of the two expressions are true

---
+++ js/xor.js
@@ -0,0 +1,9 @@
+//. The exclusive or evaluates two booleans. It then returns true if exactly one of the two expressions are true, false otherwise.
+
+
+
+
+
+//Solution
+function xor(a, b) {
+    return a!=b ? true : false}
