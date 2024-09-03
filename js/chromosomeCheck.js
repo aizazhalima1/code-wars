a0b91052ba9fe65e1e1d9a9565398227f78644cf
aizazhalima1 <halimaaizaz@hotmail.com>
2024-09-03 00:17:21 -0400

    Determine sex based on chromosomes

---
+++ js/chromosomeCheck.js
@@ -0,0 +1,12 @@
+//Determine offspring sex based on genes XX and XY chromosomes
+
+
+
+
+
+//Solution
+function chromosomeCheck(sperm) {
+    return sperm.includes('Y')? "Congratulations! You're going to have a son.":
+    "Congratulations! You're going to have a daughter."
+    
+  }
