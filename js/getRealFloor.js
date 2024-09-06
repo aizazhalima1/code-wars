662d8c335c2638ab47e88a3a59f0edbd1a00dc24
aizazhalima1 <halimaaizaz@hotmail.com>
2024-09-06 17:36:43 -0400

    Change American floor number to european floor number

---
+++ js/getRealFloor.js
@@ -0,0 +1,18 @@
+//Write a function that given a floor in the american system returns the floor in the european system
+
+
+
+
+
+//Solution
+function getRealFloor(n) {
+    if(n<=0){
+      return n
+    }
+    else if(n>13){
+      return n-2
+    }
+    else{
+      return n-1
+    } 
+  }
