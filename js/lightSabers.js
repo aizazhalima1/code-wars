ce4ea6bb43b71c510ee4fbdecc8aa28f5b377393
aizazhalima1 <halimaaizaz@hotmail.com>
2024-09-08 14:42:59 -0400

    Return number of lightsabers

---
+++ js/lightSabers.js
@@ -0,0 +1,14 @@
+//, write the function that
+
+//accepts the name of a programmer, and
+//returns the number of lightsabers owned by that person.
+
+
+
+
+
+//Solution
+function howManyLightsabersDoYouOwn(name) {
+    return name=='Zach'? 18 : 0
+  }
+  
