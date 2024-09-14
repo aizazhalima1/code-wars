434a112f866d9ec81574e121d7ab475ef01f25ed
aizazhalima1 <halimaaizaz@hotmail.com>
2024-09-14 11:48:52 -0400

    Return total goals

---
+++ js/goals.js
@@ -0,0 +1,10 @@
+//Complete the function to return his total number of goals in all three leagues.
+
+
+
+
+
+//Solution
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals
+   }
