00fb198c25efe3e262ccab69f944b73190dab8a8
aizazhalima1 <halimaaizaz@hotmail.com>
2024-09-17 13:11:51 -0400

    Return number of litres Nathan will drink

---
+++ js/litres.js
@@ -0,0 +1,10 @@
+//You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
+
+
+
+
+
+//Solution
+function litres(time) {
+    return Math.floor(time*0.5)
+  }
