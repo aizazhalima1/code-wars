bd07ba345406a0d18a5d2afffc22ce90a31cdeb1
aizazhalima1 <halimaaizaz@hotmail.com>
2024-09-30 14:02:08 -0400

    Return length of shortest word

---
+++ js/findShort.js
@@ -0,0 +1,12 @@
+//Simple, given a string of words, return the length of the shortest word(s).
+
+//String will never be empty and you do not need to account for different data types.
+
+
+
+
+
+//Solution
+function findShort(s){
+    return s.split(' ').map((x)=>x.length).sort((a,b)=>a-b)[0]
+  }
