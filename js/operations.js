59e415675abef63e7646d795bc14eb82802b52b5
aizazhalima1 <halimaaizaz@hotmail.com>
2024-10-02 19:38:35 -0400

    Return result of four operations

---
+++ js/operations.js
@@ -0,0 +1,30 @@
+//Make multiple functions that will return the sum, difference, modulus, product, quotient, and the exponent respectively.
+
+
+
+
+
+//Solution
+function add(a,b){
+    return a+b
+}
+
+function divide(a,b){
+    return a/b
+}
+
+function multiply(a,b){
+    return a*b
+}
+
+function mod(a,b){
+    return a%b
+}
+   
+function exponent(a,b){
+    return a**b
+}
+    
+function subt(a,b){
+    return a-b
+}
