208318cf5b5a9cb07f20f6ca1fb95a8b96c58abd
aizazhalima1 <halimaaizaz@hotmail.com>
2024-10-18 18:22:46 -0400

    Return array of even numbers

---
+++ js/getEvenNumbers.js
@@ -0,0 +1,11 @@
+//Use the filter functionality to complete the function given.
+
+
+
+
+
+//Solution
+function getEvenNumbers(numbersArray){
+    numbersArray= numbersArray.filter((x)=>x%2==0)
+     return numbersArray
+    }
