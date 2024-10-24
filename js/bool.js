4da7f662c8a06ad81b62f0c6033755bc6bd8782e
aizazhalima1 <halimaaizaz@hotmail.com>
2024-10-24 13:20:52 -0400

    Return func wrt boolean values

---
+++ js/bool.js
@@ -0,0 +1,17 @@
+//Create a function called _if which takes 3 arguments: a value bool and 2 functions (which do not take any parameters): func1 and func2
+
+//When bool is truthy, func1 should be called, otherwise call the func2.
+
+
+
+
+
+//Solution
+function _if(bool, func1, func2) {
+    if(bool==true){
+      func1()
+    }
+    else{
+      func2()
+    }
+  }
