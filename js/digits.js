4b8b5c5102c2cef907100152cbaa0a44fa30f063
aizazhalima1 <halimaaizaz@hotmail.com>
2024-11-07 23:34:11 -0500

    Return total number of digits

---
+++ js/digits.js
@@ -0,0 +1,12 @@
+//Determine the total number of digits in the integer (n>=0) given as input to the function. 
+
+
+
+
+
+//Solution
+function digits(n) {
+    let arr=String(n).split('')
+    return arr.length
+    
+  }
